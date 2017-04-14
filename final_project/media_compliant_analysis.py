2ed357f2188f626f06b92b658ad7ba856d2b50a2
w205 <w205@ip-172-31-54-245.ec2.internal>
2017-04-14 03:39:05 +0000

    new

--- final_project/media_compliant_analysis.py
+++ final_project/media_compliant_analysis.py
@@ -3,9 +3,10 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 conn = psycopg2.connect(database="projectdatabase", user="postgres", password="pass", host="localhost", port="5432")
-cur = conn.cursor("Create table media_compliant_analysis as SELECT c.year_month, c.cleaned_company, c.complaints_count, avg(m.avgtone) AS sentiment FROM ccdata_monthly c join mediadata2 m ON (c.year_month = m.monthyear) GROUP BY c.year_month, c.cleaned_company, c.complaints_count ORDER BY c.year_month DESC")
-
-
+cur = conn.cursor()
+cur.execute("Create table media_compliant_analysis as SELECT c.year_month, c.cleaned_company, c.complaints_count, avg(m.avgtone) AS sentiment FROM ccdata_monthly c join mediadata2 m ON (c.year_month = m.monthyear) GROUP BY c.year_month, c.cleaned_company, c.complaints_count ORDER BY c.year_month DESC")
+cur.execute("create index title_index2 On ccdata_monthly (year_month)")
+cur.execute("create index title_index1 On mediadata2 (monthyear)")
 conn.commit()
 cur.close()
 conn.close()
