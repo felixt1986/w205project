809f24aa0c39b0d9196b080fe06b35292d0585da
w205 <w205@ip-172-31-54-245.ec2.internal>
2017-04-25 15:55:06 +0000

    new

--- final_project/media_complaints_analysis_5.py
+++ final_project/media_complaints_analysis_5.py
@@ -10,16 +10,38 @@ now=datetime.datetime.now()
 a="create table "
 b="complaints_media_analysis_"
 c=str(now.year)+now.strftime('%m')
-d=" as SELECT c.year_month, c.cleaned_company, c.complaints_count, sum(m.numsources) as mentions, avg (avgtone) as sentiment FROM ccdata_y2017_update c join mediadata_2017_update m on c.year_month = m.monthyear WHERE (m.sourceurl like  lower('%'||c.cleaned_company||'%') AND c.year_month = '"
+c1 =str(now.year)
+d=" as SELECT c.year_month, c.cleaned_company, c.complaints_count, sum(m.numsources) as mentions, avg (avgtone) as sentiment FROM ccdata_y"
+d1="_update c join mediadata_"
+d2="_update m on c.year_month = m.monthyear WHERE (m.sourceurl like  lower('%'||c.cleaned_company||'%') AND c.year_month = '"
 e="') GROUP BY c.year_month, c.cleaned_company, c.complaints_count order by c.complaints_count DESC"
 f="drop table if exists "
 
+#Run once off when initial setup is done
+cur.execute(a+b+"201601"+d+"2016"+d1+"2016"+d2+"201601"+e)
+cur.execute(a+b+"201602"+d+"2016"+d1+"2016"+d2+"201602"+e)
+cur.execute(a+b+"201603"+d+"2016"+d1+"2016"+d2+"201603"+e)
+cur.execute(a+b+"201604"+d+"2016"+d1+"2016"+d2+"201604"+e)
+cur.execute(a+b+"201605"+d+"2016"+d1+"2016"+d2+"201605"+e)
+cur.execute(a+b+"201606"+d+"2016"+d1+"2016"+d2+"201606"+e)
+cur.execute(a+b+"201607"+d+"2016"+d1+"2016"+d2+"201607"+e)
+cur.execute(a+b+"201608"+d+"2016"+d1+"2016"+d2+"201608"+e)
+cur.execute(a+b+"201609"+d+"2016"+d1+"2016"+d2+"201609"+e)
+cur.execute(a+b+"201610"+d+"2016"+d1+"2016"+d2+"201610"+e)
+cur.execute(a+b+"201611"+d+"2016"+d1+"2016"+d2+"201611"+e)
+cur.execute(a+b+"201612"+d+"2016"+d1+"2016"+d2+"201612"+e)
+cur.execute(a+b+"201701"+d+"2017"+d1+"2017"+d2+"201701"+e)
+cur.execute(a+b+"201702"+d+"2017"+d1+"2017"+d2+"201702"+e)
+cur.execute(a+b+"201703"+d+"2017"+d1+"2017"+d2+"201703"+e)
+cur.execute(a+b+"201704"+d+"2017"+d1+"2017"+d2+"201704"+e)
+cur.execute("create table complaints_media_analysis_Combined as select * from (select * from complaints_media_analysis_201704 union select * from complaints_media_analysis_201703 union select * from complaints_media_analysis_201702 union select * from complaints_media_analysis_201701 union select * from complaints_media_analysis_201612 union select * from complaints_media_analysis_201611 union select * from complaints_media_analysis_201610 union select * from complaints_media_analysis_201609 union select * from complaints_media_analysis_201608 union select * from complaints_media_analysis_201607 union select * from complaints_media_analysis_201606 union select * from complaints_media_analysis_201605 union select * from complaints_media_analysis_201604 union select * from complaints_media_analysis_201603 union select * from complaints_media_analysis_201602 union select * from complaints_media_analysis_201601) u order by u.year_month")
 
+#Run Monthly
 cur.execute(f+b+c)
-cur.execute(a+b+c+d+c+e)
-cur.execute("drop table if exists complaints_media_analysis_Combined")
-
-cur.execute("create table complaints_media_analysis_Combined as select * from (select * from complaints_media_analysis_201704 union select * from complaints_media_analysis_201703 union select * from complaints_media_analysis_201702 union select * from complaints_media_analysis_201701 union select * from complaints_media_analysis_201612 union select * from complaints_media_analysis_201611 union select * from complaints_media_analysis_201610 union select * from complaints_media_analysis_201609 union select * from complaints_media_analysis_201608 union select * from complaints_media_analysis_201607 union select * from complaints_media_analysis_201606 union select * from complaints_media_analysis_201605 union select * from complaints_media_analysis_201604 union select * from complaints_media_analysis_201603 union select * from complaints_media_analysis_201602 union select * from complaints_media_analysis_201601) u order by u.year_month")  
+cur.execute(a+b+c+d+c1+d1+c1+d2+c+e)
+cur.execute("alter table complaints_media_analysis_Combined Rename to temp")
+cur.execute("create table complaints_media_analysis_Combined as select * from (select * from complaints_media_analysis_201704 union select * from temp) u order by u.year_month")  
+cur.execute("drop table if exists temp")
 
 conn.commit()
 cur.close()
